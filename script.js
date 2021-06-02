ef6e37094ae3b55f73c00f851d83126e739d9570
unequalv <unequalv@gmail.com>
2021-06-02 22:09:01 +0530

    Added Operatants

--- script.js
+++ script.js
@@ -1,6 +1,72 @@
 
-let arr;
+let arr = [0];
+let a = 1;
 
-function myFunction() {
-    document.getElementById("num1").innerHTML = "0";
+
+function myfunction() {
+    arr[0] = 0;
+    document.getElementById("num1").innerHTML = arr[0];
 }
+
+function myfunctiond() {
+    let endNumber = arr[0] % 10;
+    arr[0] = arr[0] - endNumber;
+    arr[0] = arr[0] / 10;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunction7() {
+    arr[0] = arr[0] * 10 + 7;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunction1() {
+    arr[0] = arr[0] * 10 + 1;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunction2() {
+    arr[0] = arr[0] * 10 + 2;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunction3() {
+    arr[0] = arr[0] * 10 + 3;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunction4() {
+    arr[0] = arr[0] * 10 + 4;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunction5() {
+    arr[0] = arr[0] * 10 + 5;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunction6() {
+    arr[0] = arr[0] * 10 + 6;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunction8() {
+    arr[0] = arr[0] * 10 + 8;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+function myfunction9() {
+    arr[0] = arr[0] * 10 + 9;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+function myfunction0() {
+    arr[0] = arr[0] * 10 + 0;
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+function myfunctiondot() {
+    arr[0] = arr[0]
+    document.getElementById("num1").innerHTML = arr[0];
+}
+
+
+
